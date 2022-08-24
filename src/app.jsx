94a6b147bf882a719f2d966d38b4a352e72b222b
makomaster2 <82962727+makomaster2@users.noreply.github.com>
2022-08-24 13:45:30 -0500

    Formatting, Tweaked styles, Working on autoclicker

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-	// function testClick() {
-	// 	console.log('You clicked a div!!');
-	// 	numberOfClicks++;
-	// 	console.log(numberOfClicks);
-	// }
-
 	const [count, setCount] = useState(0);
-	const [countPerClick, setCountPerClick] = useState(1);
 
+	// PER CLICK UPGRADE #1
+	const [cookiesPerClick, setCookiesPerClick] = useState(1);
+	const [clickUpgradeOneCost, setClickUpgradeOneCost] = useState(10);
+
+	// AUTOCLICK UPGRADE #1
+	// const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
+	// const [autoClickUpgradeCost, setAutoClickUpgradeCost] = useState(100);
+	// const [autoClickEnabled, setAutoClickEnabled] = useState(false);
+
+	// PER CLICK UPGRADE #1
 	function upgradeOne() {
-		if (count >= 10 && countPerClick == 1) {
-			setCountPerClick(countPerClick + 1);
+		if (count >= clickUpgradeOneCost) {
+			setCookiesPerClick(cookiesPerClick + 1);
+			setCount(count - clickUpgradeOneCost);
+			setClickUpgradeOneCost(clickUpgradeOneCost * 2);
 		}
 	}
 
+	// AUTOCLICKER #1
+	// console.log('component rendered');
+	// useEffect(() => {
+	// 	let interval;
+	// 	if (autoClickEnabled) {
+	// 		interval = setInterval(() => {
+	// 			setCount(count + 1);
+	// 			// setAutoClickEnabled(false);
+	// 		}, 1000);
+	// 	}
+	// 	return () => clearInterval(interval);
+	// }, [autoClickEnabled]);
+
 	return (
-		<div>
+		<div id='mainContainer'>
 			<div className='container'>
-				<div className='numberOfClicks'>{count}</div>
+				{/* <div id='cookiesPerSecond'>
+					COOKIES PER SECOND:
+					<br /> {cookiesPerSecond}
+				</div> */}
+				<div id='cookiesPerClick'>
+					COOKIES PER CLICK:
+					<br />
+					{cookiesPerClick}
+				</div>
+				<div className='numberOfClicks'>COOKIES: {count}</div>
 				<div
-					id='clicker'
-					onClick={() => setCount(count + countPerClick)}
+					id='cookie'
+					onClick={() => setCount(count + cookiesPerClick)}
 				>
-					Clicker
+					COOKIE
 				</div>
 				<div className='upgrade' onClick={upgradeOne}>
 					UPGRADE #1
+					<br />
+					Cost: {clickUpgradeOneCost} COOKIES
 				</div>
+				{/* <div
+					className='autoClickUpgrade'
+					onClick={() => setAutoClickEnabled(true)}
+				>
+					AutoClick Upgrade #1
+					<br />
+					Cost: {autoClickUpgradeCost}
+				</div> */}
 			</div>
 		</div>
 	);
